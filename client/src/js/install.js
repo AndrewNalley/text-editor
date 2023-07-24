a263f56354b02afa0c51f92c3408148bf9ec123f
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-07-24 11:49:54 -0500

    complete install.js logic

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,22 @@
 const butInstall = document.getElementById('buttonInstall');
-
+const installText = document.getElementById('installInfo');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    butInstall.style.visibility = 'visible';
+    installText.textContent = 'Click the button to install';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async (event) => {
+    event.prompt()
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'Installed successfully!';
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    installText.textContent = 'Installed successfully!';
+    console.log('applications installed', event);
+});
